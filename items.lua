92505c730ad64ed5503026ca55af36e32afb3e39
SkiRich <richard@szkodzinski.com>
2019-05-01 00:31:17 -0400

    Initial Commit

---
+++ items.lua
@@ -0,0 +1,14 @@
+return {
+PlaceObj('ModItemCode', {
+	'name', "AT_1Init",
+	'FileName', "Code/AT_1Init.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "AT_2Panels",
+	'FileName', "Code/AT_2Panels.lua",
+}),
+PlaceObj('ModItemCode', {
+	'name', "AT_3ModConfig",
+	'FileName', "Code/AT_3ModConfig.lua",
+}),
+}
---
+++ metadata.lua
@@ -0,0 +1,16 @@
+return PlaceObj('ModDef', {
+	'title', "Automated Tourism",
+	'id', "aCvjJmz",
+	'pops_desktop_uuid', "c515e2e3-ff0a-4dc2-91f4-c6b58431e755",
+	'pops_any_uuid', "44a35da1-f32e-4034-a8e5-4280e705a553",
+	'author', "SkiRich",
+	'version', 2,
+	'lua_revision', 233360,
+	'saved_with_revision', 244275,
+	'code', {
+		"Code/AT_1Init.lua",
+		"Code/AT_2Panels.lua",
+		"Code/AT_3ModConfig.lua",
+	},
+	'saved', 1556684765,
+})
