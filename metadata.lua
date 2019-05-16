4b64fc275b4b625645dd7f457441ad471a2c73ac
SkiRich <richard@szkodzinski.com>
2019-05-16 02:39:07 -0400

    Steam Upload

--- metadata.lua
+++ metadata.lua
@@ -2,7 +2,7 @@ return PlaceObj('ModDef', {
 	'title', "Automated Tourism",
 	'description', "Automated Tourism\nv1.0\nby SKiRich",
 	'image', "AutomatedTourism.png",
-	'last_changes', "v1.0 May 1st, 2019",
+	'last_changes', "v1.0 May 5th, 2019",
 	'ignore_files', {
 		"*.git/*",
 		"*.svn/*",
@@ -14,7 +14,7 @@ return PlaceObj('ModDef', {
 	'pops_desktop_uuid', "c515e2e3-ff0a-4dc2-91f4-c6b58431e755",
 	'pops_any_uuid', "44a35da1-f32e-4034-a8e5-4280e705a553",
 	'author', "SkiRich",
-	'version', 4,
+	'version', 6,
 	'lua_revision', 233360,
 	'saved_with_revision', 244275,
 	'code', {
@@ -22,6 +22,6 @@ return PlaceObj('ModDef', {
 		"Code/AT_2Panels.lua",
 		"Code/AT_3ModConfig.lua",
 	},
-	'saved', 1557362288,
+	'saved', 1557988164,
 	'screenshot1', "Images/AutomatedTourism.png",
 })
