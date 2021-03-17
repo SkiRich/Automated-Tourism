5c7a1b7839ed0cdd92df61ecaa93a722bcc4ab39
SkiRich <richard@szkodzinski.com>
2021-03-17 14:40:52 -0400

    Upload to Steam

--- metadata.lua
+++ metadata.lua
@@ -14,14 +14,14 @@ return PlaceObj('ModDef', {
 	'pops_desktop_uuid', "c515e2e3-ff0a-4dc2-91f4-c6b58431e755",
 	'pops_any_uuid', "44a35da1-f32e-4034-a8e5-4280e705a553",
 	'author', "SkiRich",
-	'version', 71,
+	'version', 74,
 	'lua_revision', 233360,
-	'saved_with_revision', 1001514,
+	'saved_with_revision', 1001539,
 	'code', {
 		"Code/AT_1Init.lua",
 		"Code/AT_2Panels.lua",
 		"Code/AT_3ModConfig.lua",
 	},
-	'saved', 1615874506,
+	'saved', 1616006365,
 	'screenshot1', "Images/AutomatedTourism.png",
 })
