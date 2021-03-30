070bfa1362fb194e52a0efacf7b1067400b676fc
SkiRich <richard@szkodzinski.com>
2021-03-30 16:02:16 -0400

    Replaced all tab chars with spaces
    - easier to use compare for changes

--- Code/AT_3ModConfig.lua
+++ Code/AT_3ModConfig.lua
@@ -20,40 +20,40 @@ g_ModConfigLoaded = false
 
 -- wait for mod config to load or fail out and use defaults
 local function WaitForModConfig()
-	if (not ModConfigWaitThread) or (not IsValidThread(ModConfigWaitThread)) then
-		ModConfigWaitThread = CreateRealTimeThread(function()
-	    if lf_print then print(string.format("%s WaitForModConfig Thread Started", mod_name)) end
+  if (not ModConfigWaitThread) or (not IsValidThread(ModConfigWaitThread)) then
+    ModConfigWaitThread = CreateRealTimeThread(function()
+      if lf_print then print(string.format("%s WaitForModConfig Thread Started", mod_name)) end
       local Sleep = Sleep
       local TableFind  = table.find
       local ModsLoaded = ModsLoaded
       local threadlimit = 120  -- loops to wait before fail and exit thread loop
- 		  while threadlimit > 0 do
- 		  	--check to make sure another mod didn't already set g_ModConfigLoaded
- 			  if not g_ModConfigLoaded then
- 			  	g_ModConfigLoaded = TableFind(ModsLoaded, "steam_id", ModConfig_id) or false
- 			  end -- if not g_ModConfigLoaded
- 			  if g_ModConfigLoaded and ModConfig:IsReady() then
- 			  	-- if ModConfig loaded and is in ready state then set as true
- 			  	g_ModConfigLoaded = true
- 			  	break
- 			  else
- 			    Sleep(500) -- Sleep 1/2 second
- 			  end -- if g_ModConfigLoaded
- 			  threadlimit = threadlimit - 1
- 		  end -- while
+       while threadlimit > 0 do
+         --check to make sure another mod didn't already set g_ModConfigLoaded
+         if not g_ModConfigLoaded then
+           g_ModConfigLoaded = TableFind(ModsLoaded, "steam_id", ModConfig_id) or false
+         end -- if not g_ModConfigLoaded
+         if g_ModConfigLoaded and ModConfig:IsReady() then
+           -- if ModConfig loaded and is in ready state then set as true
+           g_ModConfigLoaded = true
+           break
+         else
+           Sleep(500) -- Sleep 1/2 second
+         end -- if g_ModConfigLoaded
+         threadlimit = threadlimit - 1
+       end -- while
       if lf_print then print(string.format("%s WaitForModConfig Thread Continuing", mod_name)) end
 
       -- See if ModConfig is installed and any defaults changed
       if g_ModConfigLoaded and ModConfig:IsReady() then
 
-		    g_AT_Options.ATdismissMsg = ModConfig:Get("Automated_Tourism", "ATdismissMsg")
-		    if not g_AT_Options.ATdismissMsg then
-		    	g_AT_Options.ATnoticeDismissTime = -1
-		    else
-		    	g_AT_Options.ATnoticeDismissTime = ModConfig:Get("Automated_Tourism", "ATnoticeDismissTime") * 1000
-		    end -- if not g_AT_Options.ATdismissMsg
+        g_AT_Options.ATdismissMsg = ModConfig:Get("Automated_Tourism", "ATdismissMsg")
+        if not g_AT_Options.ATdismissMsg then
+          g_AT_Options.ATnoticeDismissTime = -1
+        else
+          g_AT_Options.ATnoticeDismissTime = ModConfig:Get("Automated_Tourism", "ATnoticeDismissTime") * 1000
+        end -- if not g_AT_Options.ATdismissMsg
 
-		    g_AT_Options.ATMaxTourists     = ModConfig:Get("Automated_Tourism", "ATMaxTourists")
+        g_AT_Options.ATMaxTourists     = ModConfig:Get("Automated_Tourism", "ATMaxTourists")
         g_AT_Options.ATvoyageWaitTime  = ModConfig:Get("Automated_Tourism", "ATvoyageWaitTime")
         g_AT_Options.ATrecallRadius    = ModConfig:Get("Automated_Tourism", "ATrecallRadius")
         g_AT_Options.ATearlyDepartures = ModConfig:Get("Automated_Tourism", "ATearlyDepartures")
@@ -63,23 +63,23 @@ local function WaitForModConfig()
 
         ModLog(string.format("%s detected ModConfig running - Setup Complete", mod_name))
       else
-      	-- PUT MOD DEFAULTS HERE OR SET THEM UP BEFORE RUNNING THIS FUNCTION ---
+        -- PUT MOD DEFAULTS HERE OR SET THEM UP BEFORE RUNNING THIS FUNCTION ---
 
-    	  if lf_print then print(string.format("**** %s - Mod Config Never Detected On Load - Using Defaults ****", mod_name)) end
-    	  ModLog(string.format("**** %s - Mod Config Never Detected On Load - Using Defaults ****", mod_name))
+        if lf_print then print(string.format("**** %s - Mod Config Never Detected On Load - Using Defaults ****", mod_name)) end
+        ModLog(string.format("**** %s - Mod Config Never Detected On Load - Using Defaults ****", mod_name))
       end -- end if g_ModConfigLoaded
       if lf_print then print(string.format("%s WaitForModConfig Thread Ended", mod_name)) end
-		end) -- thread
-	else
-		if lf_print then print(string.format("%s Error - WaitForModConfig Thread Never Ran", mod_name)) end
-		ModLog(string.format("%s Error - WaitForModConfig Thread Never Ran", mod_name))
-	end -- check to make sure thread not running
+    end) -- thread
+  else
+    if lf_print then print(string.format("%s Error - WaitForModConfig Thread Never Ran", mod_name)) end
+    ModLog(string.format("%s Error - WaitForModConfig Thread Never Ran", mod_name))
+  end -- check to make sure thread not running
 end -- WaitForModConFig
 
 ---------------------------------------------- OnMsgs -------------------------------------------------------------------
 
 function OnMsg.ModConfigReady()
-	local StringIdBase = 17764702300 -- Automated Tourism
+  local StringIdBase = 17764702300 -- Automated Tourism
 
     -- Register this mod's name and description
     ModConfig:RegisterMod("Automated_Tourism", -- ID
@@ -184,75 +184,75 @@ end -- ModConfigReady
 function OnMsg.ModConfigChanged(mod_id, option_id, value, old_value, token)
   if g_ModConfigLoaded and (mod_id == "Automated_Tourism") and (token ~= "reset") then
 
-  	-- ATdismissMsg
-  	if option_id == "ATdismissMsg" then
-  		if value then
-  			local dismissmsgtime = ModConfig:Get("Automated_Tourism", "ATnoticeDismissTime")
-  			g_AT_Options.ATnoticeDismissTime = dismissmsgtime * 1000 -- (msec)
-  			g_AT_Options.ATdismissMsg = value
-  		else
-  			g_AT_Options.ATnoticeDismissTime = -1 -- stay on screen until dismissed
-  			g_AT_Options.ATdismissMsg = value
-  		end -- if value is true
+    -- ATdismissMsg
+    if option_id == "ATdismissMsg" then
+      if value then
+        local dismissmsgtime = ModConfig:Get("Automated_Tourism", "ATnoticeDismissTime")
+        g_AT_Options.ATnoticeDismissTime = dismissmsgtime * 1000 -- (msec)
+        g_AT_Options.ATdismissMsg = value
+      else
+        g_AT_Options.ATnoticeDismissTime = -1 -- stay on screen until dismissed
+        g_AT_Options.ATdismissMsg = value
+      end -- if value is true
     end -- ATPdismissMsg
 
-  	-- ATnoticeDismissTime
-  	if option_id == "ATnoticeDismissTime" and g_AT_Options.ATdismissMsg then
+    -- ATnoticeDismissTime
+    if option_id == "ATnoticeDismissTime" and g_AT_Options.ATdismissMsg then
       g_AT_Options.ATnoticeDismissTime = value * 1000 -- in msecs
     end -- ATPdismissMsg
 
-  	-- ATMaxTourists
-  	if option_id == "ATMaxTourists" then
+    -- ATMaxTourists
+    if option_id == "ATMaxTourists" then
       g_AT_Options.ATMaxTourists = value -- maximum number of tourists per rocket
     end -- ATMaxTourists
 
-  	-- ATvoyageWaitTime
-  	if option_id == "ATvoyageWaitTime" then
+    -- ATvoyageWaitTime
+    if option_id == "ATvoyageWaitTime" then
       g_AT_Options.ATvoyageWaitTime = value -- sols to wait between voyages
     end -- ATvoyageWaitTime
 
     --ATrecallRadius
-  	if option_id == "ATrecallRadius" then
+    if option_id == "ATrecallRadius" then
       g_AT_Options.ATrecallRadius = value -- display tourist recall circle
       if not value then
-      	-- turn off all recall boundaries
-      	local rockets = (UICity and UICity.labels.SupplyRocket) or ""
-      	for i = 1, #rockets do
-      		if (rockets[i].AT_RecallRadiusMode == "Mod Config Set") and rockets[i].AT_touristBoundary and IsValid(rockets[i].AT_touristBoundary) then ATtoggleTouristBoundary(rockets[i], false) end
-      	end -- for i
+        -- turn off all recall boundaries
+        local rockets = (UICity and UICity.labels.SupplyRocket) or ""
+        for i = 1, #rockets do
+          if (rockets[i].AT_RecallRadiusMode == "Mod Config Set") and rockets[i].AT_touristBoundary and IsValid(rockets[i].AT_touristBoundary) then ATtoggleTouristBoundary(rockets[i], false) end
+        end -- for i
       else
-      	-- turn on all recall boundaries
-      	local rockets = (UICity and UICity.labels.SupplyRocket) or ""
-      	for i = 1, #rockets do
-      		if (rockets[i].AT_RecallRadiusMode == "Mod Config Set") and rockets[i].AT_enabled and not rockets[i].AT_touristBoundary and not IsValid(rockets[i].AT_touristBoundary) then ATtoggleTouristBoundary(rockets[i], true) end
-      	end -- for i
+        -- turn on all recall boundaries
+        local rockets = (UICity and UICity.labels.SupplyRocket) or ""
+        for i = 1, #rockets do
+          if (rockets[i].AT_RecallRadiusMode == "Mod Config Set") and rockets[i].AT_enabled and not rockets[i].AT_touristBoundary and not IsValid(rockets[i].AT_touristBoundary) then ATtoggleTouristBoundary(rockets[i], true) end
+        end -- for i
       end -- if not value
     end -- ATrecallRadius
 
     -- ATearlyDepartures
-  	if option_id == "ATearlyDepartures" then
+    if option_id == "ATearlyDepartures" then
       g_AT_Options.ATearlyDepartures = value -- allow early departures
     end -- ATearlyDepartures
 
     -- g_AT_Options.ATstripSpecialty
-  	if option_id == "ATstripSpecialty" then
+    if option_id == "ATstripSpecialty" then
       g_AT_Options.ATstripSpecialty = value -- strip specialties
     end -- g_AT_Options.ATstripSpecialty
 
     --g_AT_Options.ATpreventDepart - default is true
-  	if option_id == "ATpreventDepart" then
-  		g_AT_Options.ATpreventDepart = value -- prevent regular rockets from being used by tourists or earthsick
-  		-- code to start and stop all the generateddeparture threads
-  		-- default is true - if flipping value to false we need to restart all the threads on non AT rockets
+    if option_id == "ATpreventDepart" then
+      g_AT_Options.ATpreventDepart = value -- prevent regular rockets from being used by tourists or earthsick
+      -- code to start and stop all the generateddeparture threads
+      -- default is true - if flipping value to false we need to restart all the threads on non AT rockets
       if not g_AT_Options.ATpreventDepart then
-      	ATStartDepartureThreads() -- start all departure threads, except on AT_enabled rockets
+        ATStartDepartureThreads() -- start all departure threads, except on AT_enabled rockets
       else
-      	ATStopDepartureThreads() -- if flipping value back to true we need to stop any generateddeparture threads
+        ATStopDepartureThreads() -- if flipping value back to true we need to stop any generateddeparture threads
       end -- if not g_AT_Options.ATpreventDepar
     end -- g_AT_Options.ATpreventDepart
 
     -- g_AT_Options.ATfoodPerTourist
-  	if option_id == "ATfoodPerTourist" then
+    if option_id == "ATfoodPerTourist" then
       g_AT_Options.ATfoodPerTourist = value -- food per tourist en route to mars
     end -- g_AT_Options.ATfoodPerTourist
 
@@ -282,7 +282,7 @@ local function SRDailyPopup()
         } -- params
         local choice = WaitPopupNotification(false, params)
         if choice == 1 then
-        	OpenUrl("https://steamcommunity.com/sharedfiles/filedetails/?id=" .. steam_id, true)
+          OpenUrl("https://steamcommunity.com/sharedfiles/filedetails/?id=" .. steam_id, true)
         end -- if statement
     end ) -- CreateRealTimeThread
 end -- function end
